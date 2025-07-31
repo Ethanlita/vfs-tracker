4d38915e0805ce611e5d950600c95cf46c131d62
Ethanlita <lita@ethanlita.com>
2025-07-31 17:46:47 +0800

    [run-fix] Add permission for github actions [run-fix] Add postcss.config.js [bug-fix] Tailwind CSS installed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Use relative paths for GitHub Pages
+  base: '/', // Use relative paths for GitHub Pages
   plugins: [react()],
 })
